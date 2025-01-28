213676aab7b3ae074e80a7025d1bfbbf84b0d4b2
GaronMiguelA <miguel.garzon.7e7@itb.cat>
2025-01-28 19:51:29 +0100

    commit

--- pinia-course/src/App.vue
+++ pinia-course/src/App.vue
@@ -1,7 +1,24 @@
 <script setup>
 import TheHeader from "@/components/TheHeader.vue";
 import ProductCard from "@/components/ProductCard.vue";
-import products from "@/data/products.json";
+import { useProductStore } from "./store/useProductStore";
+import { storeToRefs } from "pinia";
+import { useCartStore } from "./store/CartStore";
+ 
+useProductStore();
+
+const productStore = useProductStore();
+const cartStore = useCartStore();
+productStore.fill();
+
+const addToCart = (count, product) => {
+  count = parseInt(count)
+  for (let i = 0; i < count; i++) {
+    cartStore.items.push({ product });
+  }
+
+};
+
 </script>
 
 <template>
@@ -9,10 +26,11 @@ import products from "@/data/products.json";
     <TheHeader />
     <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
       <ProductCard
-        v-for="product in products"
+        v-for="product in productStore.products"
         :key="product.name"
         :product="product"
-      />
+        @addToCart="adToCart($event, product)"
+        />
     </ul>
   </div>
 </template>
