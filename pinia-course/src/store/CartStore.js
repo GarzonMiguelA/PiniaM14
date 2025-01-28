1194e3e1aea4615f986f2f7f62f506d45d88c3db
GaronMiguelA <miguel.garzon.7e7@itb.cat>
2025-01-28 19:51:39 +0100

    c

---
+++ pinia-course/src/store/CartStore.js
@@ -0,0 +1,8 @@
+import {defineStore} from 'pinia'
+export const useCartStore = defineStore("CartStore", {
+    state: () => {
+        return {
+            items: [],
+        }
+    }
+});
--- pinia-course/src/store/useProductStore.js
+++ pinia-course/src/store/useProductStore.js
@@ -1,3 +1,22 @@
 import {defineStore} from 'pinia'
+import products from '@/data/products'
 
 export const useProductStore = defineStore("ProductStore", {
+
+    //state
+    state: () => {
+        return {
+            products: []
+        }
+    },
+  
+    //actions
+    actions: {
+        async fill() {
+            this.products = (await import("@/data/products.json")).default;
+        }
+    },
+  
+    //getters
+    
+})
